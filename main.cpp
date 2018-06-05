43452bb38ddc56e130f65e88e4a53247a8589f55
dinies <edoardo.ghini@live.it>
2018-06-05 15:08:07 +0200

    excluding the boost part of the code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "defs.hpp"
 
-#include <tuple/tuple.hpp>
+//#include <tuple/tuple.hpp>
 
 using namespace ProbabilisticModelling;
 using namespace std;
@@ -14,15 +14,15 @@ void colorPoint(RGBImage& img, int row , int col){
 
 int main(int argc, char** argv){
 
-  std::cout << "debug\n";
-  std::cout << "PATH=" << getenv("PATH") << std::endl;
+//  std::cout << "debug\n";
+// std::cout << "PATH=" << getenv("PATH") << std::endl;
   int rows=480;
   int cols=640;
   int num_pos=100;
 
   Vector2iVector pointPositions(num_pos);
 
-  std::cout << "debug\n";
+ // std::cout << "debug\n";
   for (size_t i=0; i<pointPositions.size(); i++){
     pointPositions[i]=Eigen::Vector2i(rows*drand48(), cols*drand48());
   }
@@ -30,17 +30,17 @@ int main(int argc, char** argv){
   RGBImage shown_image;
   shown_image.create(rows, cols);
   shown_image=cv::Vec3b(255,0,0);
-
+//
   cv::namedWindow("distance map");
-    //test boost
-    std::cout << "debug\n" << endl;
-    std::vector<boost::tuple<double, double, double, double> > pts_A;
-    pts_A.push_back(boost::make_tuple(
-                                      1,
-                                      0,
-                                      1,
-                                      1
-                                      ));
+//    //test boost
+//    std::cout << "debug\n" << endl;
+//    std::vector<boost::tuple<double, double, double, double> > pts_A;
+//    pts_A.push_back(boost::make_tuple(
+//                                      1,
+//                                      0,
+//                                      1,
+//                                      1
+//                                      ));
 
   for (size_t j=0; j<pointPositions.size(); j++){
     const Eigen::Vector2i& point=pointPositions[j];
@@ -48,6 +48,7 @@ int main(int argc, char** argv){
     int c=point.y();
     colorPoint(shown_image, r, c);
     cv::imshow("distance map", shown_image);
+    std::cout <<  j  << endl;
     cv::waitKey(1);
   }
 }
